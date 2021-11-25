33181d3102051df807c689fdb1d6dd818825dd83
MrNickev <nikitaz2003@yandex.ru>
2021-11-25 15:38:55 +0500

    Выполнено задание
    Написаны доктесты, в readme вставлены картинки

--- filter.py
+++ filter.py
@@ -2,47 +2,72 @@ from PIL import Image
 import numpy as np
 
 
-def getAreaAverage(arr, startRow, startCol):
+def getAreaAverage(arr, startRow, startCol, width, height):
+    """
+    Return average value of area. Sizes of area are in cell_size
+
+    :param arr: array
+    :param startRow: int
+    :param startCol: int
+    :param width: int
+    :param height: int
+    :return: float
+
+    >>> getAreaAverage([[[100, 100, 100], [200, 200, 200]], [[200, 200, 200], [100, 100, 100]]], 0, 0, 2, 2)
+    450
+    """
     average = 0
-    for row in range(startRow, startRow + cell_size[0]):
-        for col in range(startCol, startCol + cell_size[1]):
+    for row in range(startRow, startRow + height):
+        for col in range(startCol, startCol + height):
             n1 = int(arr[row][col][0])
             n2 = int(arr[row][col][1])
             n3 = int(arr[row][col][2])
             M = n1 + n2 + n3
             average += M
-    average = int(average // (cell_size[0] * cell_size[1]))
+    average = int(average // (width * height))
     return average
 
 
-def changeAreaColor(arr, startRow, staartCol, average):
-    for row in range(startRow, startRow + cell_size[0]):
-        for col in range(staartCol, staartCol + cell_size[1]):
+def changeAreaColor(arr, startRow, staartCol, average, width, height, step):
+    """
+    Change pixels for average in area
+    :param arr: array
+    :param startRow: int
+    :param staartCol: int
+    :param average: float
+    :param width: int
+    :param height: int
+    :param step: int
+    """
+    for row in range(startRow, startRow + width):
+        for col in range(staartCol, staartCol + height):
             arr[row][col][0] = int(average // step) * step // 3
             arr[row][col][1] = int(average // step) * step // 3
             arr[row][col][2] = int(average // step) * step // 3
 
 
-def createGreyPixelArtFromImage():
-    arr = np.array(img)
+def createGreyPixelArtFromImage(image, width, height, step):
+    arr = np.array(image)
     a = len(arr)
     a1 = len(arr[1])
     image_row = 0
     while image_row < a - 1:
         image_col = 0
         while image_col < a1 - 1:
-            average = getAreaAverage(arr, image_row, image_col)
-            changeAreaColor(arr, image_row, image_col, average)
-            image_col = image_col + cell_size[0]
-        image_row = image_row + cell_size[1]
+            average = getAreaAverage(arr, image_row, image_col, width, height)
+            changeAreaColor(arr, image_row, image_col, average, width, height, step)
+            image_col = image_col + width
+        image_row = image_row + height
     res = Image.fromarray(arr)
     res.save('res.jpg')
 
-image_name = input("Введите название изображения для преобразования: ")
-img = Image.open(image_name)
-grey_gradations = int(input("Количество градаций серого: "))
-size = input("Введите размер мозайки в формате 5*5: ")
-cell_size = list(map(int, size.split('*')))
-step = 255 // grey_gradations
-createGreyPixelArtFromImage()
+def main():
+    image_name = input("Введите название изображения для преобразования: ")
+    img = Image.open(image_name)
+    grey_gradations = int(input("Количество градаций серого: "))
+    size = input("Введите размер мозайки в формате 5*5: ")
+    cell_size = list(map(int, size.split('*')))
+    step = 255 // grey_gradations
+    createGreyPixelArtFromImage(img, cell_size[0], cell_size[1], step)
 
+main()
---
+++ filter_with_filename.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import numpy as np
+
+
+def getAreaAverage(arr, startRow, startCol):
+    average = 0
+    for row in range(startRow, startRow + cell_size[0]):
+        for col in range(startCol, startCol + cell_size[1]):
+            n1 = int(arr[row][col][0])
+            n2 = int(arr[row][col][1])
+            n3 = int(arr[row][col][2])
+            M = n1 + n2 + n3
+            average += M
+    average = int(average // (cell_size[0] * cell_size[1]))
+    return average
+
+
+def changeAreaColor(arr, startRow, staartCol, average):
+    for row in range(startRow, startRow + cell_size[0]):
+        for col in range(staartCol, staartCol + cell_size[1]):
+            arr[row][col][0] = int(average // step) * step // 3
+            arr[row][col][1] = int(average // step) * step // 3
+            arr[row][col][2] = int(average // step) * step // 3
+
+
+def createGreyPixelArtFromImage():
+    arr = np.array(img)
+    a = len(arr)
+    a1 = len(arr[1])
+    image_row = 0
+    while image_row < a - 1:
+        image_col = 0
+        while image_col < a1 - 1:
+            average = getAreaAverage(arr, image_row, image_col)
+            changeAreaColor(arr, image_row, image_col, average)
+            image_col = image_col + cell_size[0]
+        image_row = image_row + cell_size[1]
+    res = Image.fromarray(arr)
+    res.save('res.jpg')
+
+img = Image.open("img2.jpg")
+grey_gradations = 50
+cell_size = [10, 10]
+step = 255 // grey_gradations
+createGreyPixelArtFromImage()
+
